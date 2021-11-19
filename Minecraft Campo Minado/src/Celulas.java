ac205bfbefcf3d3052cc90da7a2dd784b783d8f8
LucasFig913 <62517521+LucasFig913@users.noreply.github.com>
2021-11-19 12:33:42 -0300

    Add files via upload

--- Minecraft Campo Minado/src/Celulas.java
+++ Minecraft Campo Minado/src/Celulas.java
@@ -1,4 +1,38 @@
 
 public class Celulas {
-
+	private Boolean bomb;
+	private Boolean flagged;
+	private int x,y;
+	
+	public void isBomb() {
+		
+	}
+	public void flag() {
+		
+	}
+	
+	public Boolean getBomb() {
+		return this.bomb;
+	}
+	public void setBomb(Boolean sBomb) {
+		this.bomb = sBomb;
+	}
+	public Boolean getFlagged() {
+		return this.flagged;
+	}
+	public void setVivo(Boolean sFlagged) {
+		this.flagged = sFlagged;
+	}
+	public int getX() {
+		return this.x;
+	}
+	public void setX(int sX) {
+		this.x = sX;
+	}
+	public int getY() {
+		return this.y;
+	}
+	public void setY(int sY) {
+		this.y = sY;
+	}
 }
