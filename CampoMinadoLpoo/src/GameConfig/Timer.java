a6c24548425ae30ad4f0689963a7f813680e2ea5
pedinvini <92653352+pedinvini@users.noreply.github.com>
2021-12-07 23:42:18 -0300

    Add files via upload

--- CampoMinadoLpoo/src/GameConfig/Timer.java
+++ CampoMinadoLpoo/src/GameConfig/Timer.java
@@ -1,25 +1,36 @@
 package GameConfig;
 
-
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class Timer implements Runnable{
-    private boolean exit = false;
-    private int seconds;
-    @Override
-    public void run() {
-    	while (!exit) {
-            seconds++;
-            if(seconds==990)
-                seconds=0;
-            try {
-                Thread.sleep(1000);
-            } catch (Exception e) {
+import Board.Cell;
+import Board.Handler;
 
-            }
-        }
-    }
-    public void stop(){
-        exit = true;
-    }
-}
+public class Timer implements Runnable{
+		private static boolean exit = false;
+	    private static int seconds;
+	   
+	    public int run(int benga) {
+	    	while (!exit) {
+	            this.seconds++;
+	            if(this.seconds==benga)
+	            	System.exit(0);
+	            try {
+	                Thread.sleep(1000);
+	            } catch (Exception e) {
+	            	
+	            }
+	            System.out.println(this.seconds);
+	        	
+	    	}
+			return this.seconds;
+	    }
+	    public static void stop(){
+	   
+	    	exit = true;
+	    }
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
+		}
+	}
