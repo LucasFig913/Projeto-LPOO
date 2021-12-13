2c986fd13538803e7013c4f6361599e5592996d8
LucasFig913 <62517521+LucasFig913@users.noreply.github.com>
2021-12-13 11:15:05 -0300

    Update Game.java

--- CampoMinadoLpoo/src/GameConfig/Game.java
+++ CampoMinadoLpoo/src/GameConfig/Game.java
@@ -1,14 +1,23 @@
 package GameConfig;
 import java.awt.EventQueue;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.*;
+import java.awt.event.*;
 import java.awt.Graphics;
 import java.io.File;
 import java.io.IOException;
-
+import java.util.Objects;
+import java.util.Scanner;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 
 import Board.Handler;
 import GameConfig.Menu;
@@ -18,57 +27,27 @@ public class Game {
     public static int WIDTH = 720, HEIGHT = 720;
     public static int GRIDSIZE = 15;
     public static int MINECOUNT = (int) Math.round(GRIDSIZE * GRIDSIZE * .1);
-    public static int difficulty = 1;
-    public static String gameState;
+    public static int key = 1;
 
     public Handler handler = new Handler();
 
-
+    
     public Game() {
-        if(difficulty == 1) {
+        	
+    	if(key == 1) {
     		GRIDSIZE = 15;
-    	}else if(difficulty == 2){
+    	}else if(key == 2){
     		GRIDSIZE = 20;
-    	}else if(difficulty == 3) {
+    	}else if(key == 3) {
     		GRIDSIZE = 25;
     	}
     	new Window(WIDTH, HEIGHT, GRIDSIZE, "Campo minado - ", this, handler);  
         Window.update(0);
+        
     }
     
-    public static void update() {
-    	gameState = "MENU";
-    	if(gameState == "NORMAL") {
-            new Game();
-            System.out.println("foi");
-    	}else if(gameState == "MENU") {
-    		EventQueue.invokeLater(new Runnable() {
-    			public void run() {
-    				try {
-    					Menu frame = new Menu();
-    					frame.setVisible(true);
-    				} catch (Exception e) {
-    					e.printStackTrace();
-    				}
-    			}
-    		});
-    	}
-    }
-    
-
-    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-    	update();
-    	
-    	/*File file = new File("bomba.wav");
-    	AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
-    	Clip clip = AudioSystem.getClip();
-    	clip.open(audioStream);
-    	
-    	clip.start();*/
-    	
-        	
-
-    }
+       
 
 }
+
 
