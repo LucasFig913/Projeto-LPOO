07d9cdaf166582caaef9d4abd20e52307aea8c23
pedinvini <92653352+pedinvini@users.noreply.github.com>
2021-12-15 14:24:35 -0300

    Add files via upload

--- CampoMinadoLpoo/src/GameConfig/Timer.java
+++ CampoMinadoLpoo/src/GameConfig/Timer.java
@@ -1,47 +1,52 @@
 package GameConfig;
 
 import javax.swing.JOptionPane;
+import java.lang.Runnable;
 
 import Exceptions.ExcededTime;
 
 public class Timer implements Runnable{
-		private static boolean exit = false;
+		private static boolean exit;
 	    private static int seconds;
-	    public String seconds2;
-	   
+
 	    public static int getSeconds() {
 			return seconds;
 		}
 		public void setSeconds(int seconds) {
 			Timer.seconds = seconds;
 		}
-		public int run(int x) {
-	    	while (!exit) {
-	            Timer.seconds++;
-		         if(Timer.seconds == x){
-	            	try {
-		            	throw new ExcededTime(x);	            	
-		            }catch(ExcededTime e) {
-		            	JOptionPane.showMessageDialog(null,"Exceded Time");		       
-		            	exit = true;
+		public static Runnable timer = new Runnable() {
+			public void run() {
+		           while(!exit) {
+					Timer.seconds++;
+			         if(Timer.seconds == 999){
+		            	try {
+			            	throw new ExcededTime(999);	            	
+			            }catch(ExcededTime e) {
+			            	JOptionPane.showMessageDialog(null,"Exceded Time");		       
+			            	exit = true;
+			            }
+			         }
+		            try {
+		                Thread.sleep(1000);
+		            } catch (Exception e) {
+		            	
 		            }
-		         }
-	            try {
-	                Thread.sleep(1000);
-	            } catch (Exception e) {
-	            	
-	            }
-	            System.out.println(Timer.seconds);
-	        	
-	    	}
-			return Timer.seconds;
-	    }
-	    public static void stop(){
-	   
-	    	exit = true;
-	    }
+		            System.out.println(Timer.seconds);
+			}
+		    }
+		};
+
+		public static boolean isExit() {
+			return exit;
+		}
+		public static void setExit(boolean exit) {
+			Timer.exit = exit;
+		}
 		@Override
 		public void run() {
+			// TODO Auto-generated method stub
 			
 		}
+
 	}
